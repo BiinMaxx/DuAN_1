8c209e34b807fe8fcf7e5420f1f74f0587714275
BiinMaxx <150342183+BiinMaxx@users.noreply.github.com>
2024-07-18 01:55:07 +0700

    Initial commit

---
+++ admin/controllers/AdminDanhMucController.php
@@ -0,0 +1,87 @@
+<?php
+    class AdminDanhMucController {
+        public $modelDanhMuc;
+
+        public function __construct(){
+            $this->modelDanhMuc = new AdminDanhMuc();
+        }
+        public function danhSachDanhMuc(){
+
+            $listDanhMuc = $this->modelDanhMuc->getAllDanhMuc();
+
+            require_once './views/danhmuc/listDanhMuc.php';
+        }
+        public function formAddDanhMuc(){
+            // Hiển thị form
+            require_once './views/danhmuc/addDanhMuc.php';
+
+        }
+        public function postAddDanhMuc(){
+            // Hiển thị xử lý dl
+            if($_SERVER['REQUEST_METHOD'] == 'POST'){
+                $ten_danh_muc = $_POST['ten_danh_muc'];
+                $mo_ta = $_POST['mo_ta'];
+
+            }
+            $error = [];
+            if (empty($ten_danh_muc)) {
+                $error['$ten_danh_muc'] = 'Tên danh mục không được để trống';
+            }
+            if (empty($error)) {
+                # code...
+                $this->modelDanhMuc->insertDanhMuc($ten_danh_muc, $mo_ta);
+                header("location:".BASE_URL_ADMIN. '?act=danh-muc');
+                exit();
+            }else{
+                require_once './views/danhmuc/addDanhMuc.php';
+            }
+        }
+        public function formEditDanhMuc(){
+            // Hiển thị form
+            $id = $_GET['id_danh_muc'];
+            $danhMuc = $this->modelDanhMuc->getOneDanhMuc($id);
+            if($danhMuc){
+                require_once './views/danhmuc/editDanhMuc.php';
+            }else{
+                header("location:".BASE_URL_ADMIN. '?act=danh-muc');
+                exit();
+            }
+           
+            
+
+        }
+        public function postEditDanhMuc(){
+            // Hiển thị xử lý dl
+            if($_SERVER['REQUEST_METHOD'] == 'POST'){
+                $id = $_POST['id'];
+                $ten_danh_muc = $_POST['ten_danh_muc'];
+                $mo_ta = $_POST['mo_ta'];
+
+            }
+            $error = [];
+            if (empty($ten_danh_muc)) {
+                $error['$ten_danh_muc'] = 'Tên danh mục không được để trống';
+            }
+            if (empty($error)) {
+                # code...
+                $this->modelDanhMuc->updateDanhMuc($id, $ten_danh_muc, $mo_ta);
+                header("location:".BASE_URL_ADMIN. '?act=danh-muc');
+                exit();
+            }else{
+                $danhMuc = ['id ' => $id, 'ten_danh_muc' => $ten_danh_muc, 'mo_ta' => $mo_ta];
+                require_once './views/danhmuc/editDanhMuc.php';
+            }
+        }
+        public function DeleteDanhMuc(){
+            $id = $_GET['id_danh_muc'];
+
+            $danhMuc = $this->modelDanhMuc->getOneDanhMuc($id);
+
+            if($danhMuc){
+                $this->modelDanhMuc->destroyDanhMuc($id);
+            }
+                header("location:".BASE_URL_ADMIN. '?act=danh-muc');
+                exit();
+            
+        }
+    }
---
+++ admin/views/danhmuc/editDanhMuc.php
@@ -0,0 +1,83 @@
+<!-- Header  -->
+<?php
+require './views/layout/header.php';
+?>
+<!-- Navbar  -->
+<?php
+require './views/layout/navbar.php';
+?>
+<!-- Main Sidebar -->
+<?php
+require './views/layout/sidebar.php';
+?>
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <section class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1>Quản lý danh mục</h1>
+        </div>
+      </div>
+    </div><!-- /.container-fluid -->
+  </section>
+
+  <!-- Main content -->
+  <section class="content">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-12">
+      <div class="card card-primary">
+              <div class="card-header">
+                <h3 class="card-title">Sửa danh mục</h3>
+              </div>
+              <!-- /.card-header -->
+              <!-- form start -->
+              <form action="<?= BASE_URL_ADMIN . '?act=sua-danh-muc' ?>" method="POST">
+
+              <input type="text" name="id" value="<?= $danhMuc['id'] ?>" hidden >
+
+                <div class="card-body">
+                  <div class="form-group">
+                    <label>Tên danh mục</label>
+                    <input type="text" class="form-control" name="ten_danh_muc" value="<?= $danhMuc['ten_danh_muc']?>" placeholder="Nhập tên danh mục">
+                    <?php
+                    if(isset($error['ten_danh_muc'])){?>
+                    <p class="text-danger"><?= $error['ten_danh_muc'] ?></p>
+                <?php   }?>
+                    
+                </div>
+                  
+                  <div class="form-group">
+                    <label>Mô tả</label>
+                    <textarea name="mo_ta" class="form-control" cols="30" rows="10" 
+                     placeholder="Nhập mô tả"><?= $danhMuc['mo_ta']?></textarea>
+                  </div>
+                </div>
+                <!-- /.card-body -->
+
+                <div class="card-footer">
+                  <button type="submit" class="btn btn-primary">Cập nhật</button>
+                </div>
+              </form>
+            </div>
+            </div>
+        <!-- /.col -->
+      </div>
+      <!-- /.row -->
+    </div>
+    <!-- /.container-fluid -->
+  </section>
+  <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+
+<!-- Footer -->
+<?php
+require './views/layout/footer.php';
+?>
+<!--End Footer -->
+</body>
+
+</html>
